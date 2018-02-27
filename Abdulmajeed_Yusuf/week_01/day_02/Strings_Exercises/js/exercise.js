9ec56faa4e40594c8a8d1a836b1b57c56e04710e
Abdulmajeed Yusuf <Abdulmajeed@Abdulmajeeds-MacBook-Air.local>
2018-02-28 10:33:37 +1100

    Calculators

---
+++ Abdulmajeed_Yusuf/week_01/day_02/Strings_Exercises/js/exercise.js
@@ -0,0 +1,8 @@
+// Create a function called DrEvil. It should take a single argument, an amount, and return '<amount> dollars', except it will add '(pinky)' at the end if the amount is 1 million. For example:
+//
+//   DrEvil(10): 10 dollars
+//   DrEvil(1000000): 1000000 dollars (pinky)
+
+const drEvil = function() {
+  if 
+}
