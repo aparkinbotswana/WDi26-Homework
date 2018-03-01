d1a112c5797593fac5b81595fe9074b40fb9716a
robo2323 <rob.adam.h@gmail.com>
2018-03-01 12:23:05 +1100

    added hangman

--- Rob_Harkness/week_01/day_03/word_guesser/main.js
+++ Rob_Harkness/week_01/day_03/word_guesser/main.js
@@ -1,10 +1,13 @@
 const word = 'test';
 const guessed = [];
 let reward = 0;
+let hangManCount = 0;
 
 for (let i = 0; i < word.length; i++) {
   guessed.push('_');
 }
+
+const hangMan = document.getElementsByClassName('body');
 
 const guessLetter = (letter) => {
   let correctGuess = false;
@@ -20,6 +23,13 @@ const guessLetter = (letter) => {
   } else {
     console.log('wrong guess');
     reward -= Math.floor(Math.random() * 10 + 1);
+    hangMan[hangManCount].style.opacity = 1;
+    hangManCount++;
+    console.log(hangManCount);
+
+    if (hangManCount === 6) {
+      console.log('game over');
+    }
   }
   console.log(guessed);
 
@@ -31,7 +41,9 @@ const guessLetter = (letter) => {
   }
 };
 
-guessLetter('e');
-guessLetter('t');
-guessLetter('n');
-guessLetter('s');
+document.getElementById('guessInput').addEventListener('input', function() {
+  guessLetter(this.value);
+  setTimeout(() => {
+    this.value = '';
+  }, 800);
+});
