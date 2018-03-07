7d30b30c7a57eb3da254060bb5ae365115186efe
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-03-07 16:40:08 +1100

    Wk02 Days 02 and 03

---
+++ Ricky_Liew/week_02/day_03/catwalk/js/default.js
@@ -0,0 +1,23 @@
+console.log(`test`);
+/*
+Weekxx Dayxx Homework | Classwork
+URL:
+
+*/
+
+/*
+Task:
+
+*/
+let cat = document.getElementsByTagName('img')[0];
+cat.style.position = 'absolute';
+cat.style.left = '0px';
+
+var makeCatWalk = function(){
+  console.log("test");
+  let oldPosition = parseInt(cat.style.left);
+  let newPosition = oldPosition + 10;
+  cat.style.left = newPosition + 'px';
+};
+
+window.setInterval(makeCatWalk, 1000);
