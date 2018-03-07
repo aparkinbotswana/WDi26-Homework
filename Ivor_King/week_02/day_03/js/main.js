73246cb5cf955e38db962e6f8688535da076e406
Ivor King <ivorking+c@gmail.com>
2018-03-08 08:11:06 +1100

    updating

---
+++ Ivor_King/week_02/day_03/js/main.js
@@ -0,0 +1,15 @@
+console.log("here");
+
+let a = document.getElementsByTagName("img")
+
+console.log(a)
+
+// const catRef = document.getElementsByTagName('img');
+
+// const catWalk = function() {
+//  window.setInterval(catRef,5);
+document.getElementsByTagName("img").style.left = "200px";
+// }  
+
+// catWalk();
+// check this
