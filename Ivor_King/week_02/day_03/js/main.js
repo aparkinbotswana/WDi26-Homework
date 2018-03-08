3d25b17e311dd6e1fdc459c15651a3211aa98d7a
Ivor King <ivorking+c@gmail.com>
2018-03-08 23:23:19 +1100

    updated cat project

--- Ivor_King/week_02/day_03/js/main.js
+++ Ivor_King/week_02/day_03/js/main.js
@@ -1,12 +1,32 @@
-var catMargin = 20;
-
+var catMargin = 0;
 var catPos = document.getElementById("catpic");
-catPos.style.marginLeft = "0";
+catPos.style.left = "0";
+var catDir = true;
+var timeVar = 300;
 
 var moveCat = function() {
-    catMargin = catMargin + 20;
-    catPos.style.marginLeft = catMargin.toString() + "px";
-    console.log(catMargin);
-}
+    if (catDir === true) {
+        catMargin = catMargin + 30;
+        catPos.style.left = catMargin.toString() + "px";
+    }
+    else {
+        catMargin = catMargin - 30;
+        catPos.style.left = catMargin.toString() + "px";
+    }
+    if (window.innerWidth - catMargin < 0 || catMargin < 0) {
+        catDir = !catDir;
+        if (catDir === true) {
+            catPos.style.transform = "scaleX(1)";
+        }
+        else {
+            catPos.style.transform = "scaleX(-1)";
+        }
+    }
+    // if (catMargin === 120) {
+    //     console.log("freezer");
+    //     catPos.src = "https://media.giphy.com/media/BK1EfIsdkKZMY/giphy.gif";
+    //     window.clearInterval(moveCat);
+    // }
+};
 
-window.setInterval(moveCat, 500);
+window.setInterval(moveCat, timeVar);
