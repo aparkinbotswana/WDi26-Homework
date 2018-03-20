9a7bd0a88cd954c964f737d7c4f4ce4544171011
Mandy Tang <mandytang@Mandys-MacBook-Pro.local>
2018-03-20 16:30:03 +1100

    working basic number of stops

--- Mandy_Tang/week_04/day_02/mta.rb
+++ Mandy_Tang/week_04/day_02/mta.rb
@@ -1,6 +1,22 @@
 
+#
+# def planTrip (startLine, start)
+#
+#   trainLines = { # This had to go inside the function, otherwise indexStart wouldn't trigger properly
+#     "L" => ["8th on L", "6th", "Union Square on L", "3rd", "1st"],
+#     "6" => ["Grand Central", "33rd", "28th on 6", "23rd on 6", "Union Square on 6", "Astor Place"],
+#     "N" => ["Times Square", "34th", "28th on N", "23rd on N", "Union Square on N", "8th on N"]
+#    }
+#
+#    indexStart = trainLines[startLine].find_index { |item| item == start}
+#
+# puts "#{indexStart}"
+#
+# end
+#
+# planTrip("L", "6th")
 
-def planTrip (startLine, start)
+def planTrip (startLine, start, finishLine, finish)
 
   trainLines = { # This had to go inside the function, otherwise indexStart wouldn't trigger properly
     "L" => ["8th on L", "6th", "Union Square on L", "3rd", "1st"],
@@ -9,9 +25,30 @@ def planTrip (startLine, start)
    }
 
    indexStart = trainLines[startLine].find_index { |item| item == start}
+   puts "#{indexStart}"
 
-puts "#{indexStart}"
+   if startLine == finishLine
+     indexTransfer = trainLines[startLine].find_index { |item| item == finish}
+      puts "#{indexTransfer}"
+   else
+     indexTransfer = trainLines[startLine].find_index { |item| item == "Union Square on #{startLine}"}
+      puts "#{indexTransfer}"
+   end
+
+   numStopsL1 = indexStart - indexTransfer
+
+   if numStopsL1 < 0
+     numStopsL1 = numStopsL1 * -1
+   end
+   puts "#{numStopsL1}"
+
+   if indexStart < indexTransfer
+
+
+
+
+   end
 
 end
 
-planTrip("L", "6th")
+planTrip("L", "8th on L", "L", "1st")
