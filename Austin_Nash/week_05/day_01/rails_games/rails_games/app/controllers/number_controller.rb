2ac64a3731dc1b241e3ec9c3ef574f6a6840f083
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-03-27 10:19:14 +1100

    tuesday

--- Austin_Nash/week_05/day_01/rails_games/rails_games/app/controllers/number_controller.rb
+++ Austin_Nash/week_05/day_01/rails_games/rails_games/app/controllers/number_controller.rb
@@ -5,10 +5,15 @@ class NumberController < ApplicationController
   end
 
   def result
-    @your_number = params[:number]
+    @your_number = params[:number].to_i
     arr = [1,2,3,4,5,6,7,8,9,10].shuffle
     @my_number = arr[0]
-    @win_lose = "You lose, try again" unless @your_number == @my_number
-  else "You Win!"
+
+    if @your_number == @my_number
+       @winner = "You win"
+    end
+    if @your_number != @my_number
+       @winner = "You lose!"
+    end
   end
 end
--- Austin_Nash/week_05/day_01/rails_games/rails_games/app/controllers/rps_controller.rb
+++ Austin_Nash/week_05/day_01/rails_games/rails_games/app/controllers/rps_controller.rb
@@ -1,5 +1,22 @@
 class RpsController < ApplicationController
   def rps
-    render :rps
   end
-end 
+
+  def result
+    @your_move = params[:rps]
+    move = ["rock", "paper", "scissors"].shuffle
+    @my_move = move[0]
+    if @your_move == @my_move
+       @winner = "you win"
+    end
+    if @your_move == "rock" && @my_move == "scissors"
+       @winner = "you win"
+    end
+    if @your_move == "paper" && @my_move == "rock"
+       @winner = "you win"
+    end
+    if @your_move == "scissors" && @my_move == "paper"
+       @winner = "you win"
+    end
+  end
+end
