4535c8836d1c339d2dd32281843aaf0e1ae1d348
Jose Juan Parra Alcolea <josejuan.parraalcolea@192-168-1-5.tpgi.com.au>
2018-03-27 19:02:21 +1100

    MTA, Games and Books homework

--- Jose_Juan_Alcolea/week_04/day_02/MTA.rb
+++ Jose_Juan_Alcolea/week_04/day_02/MTA.rb
@@ -1,42 +1,33 @@
 require 'pry'
 
-#
-# $lines = {
-# "N" = ["Times Square", "34th", "28thN", "23rd", "Union Square", "8thN"]
-# "L" = ["8th", "6th", "Union Square", "3rd", "1st"]
-# "6" = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
-# }
-#$lines
 
-
-def single_trip ( line , star_station, finish_station )
+def single_trip ( line , start_station, finish_station )
 
 
   lines = {
-  :N => ["Times Square", "34th", "28thN", "23rd", "Union Square", "8thN"],
-  :L =>  ["8th", "6th", "Union Square", "3rd", "1st"],
-  :L6 => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+  'N' => ["Times Square", "34th", "28thN", "23rd", "Union Square", "8thN"],
+  'L' =>  ["8th", "6th", "Union Square", "3rd", "1st"],
+  '6' => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
   }
 
 
-
   current_line = lines[line]
-  
-  start_position = current_line.find_index(star_station)
-  finis_position = current_line.find_index(finish_station)
+  start_position = current_line.index(start_station)
+  finish_position = current_line.index(finish_station)
 
+  if start_position < finish_position
+    p current_line[start_position .. finish_position]
+    else
+    p current_line[finish_position .. start_position].reverse
+  end
 
-  hop = current_line[start_position .. finis_position]
-return hop
-  # range
 end
 
 
-def double_trip ( start_line, star_station, finish_line, finish_station)
+
+def double_trip ( start_line, start_station, finish_line, finish_station)
 
   p single_trip  start_line, star_station, "Union Square"
   p single_trip  finish_line, "Union Square", finish_station
 
 end
-
-binding.pry
