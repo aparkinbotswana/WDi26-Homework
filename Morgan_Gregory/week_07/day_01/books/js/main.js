73fd0bc8e47bb420caa7419a2b96f94a844092af
Morgan Gregory <morgangregory@Morgans-MBP.gateway>
2018-04-09 20:53:11 +1000

    changes to book cover AJAX day 01 week 7

--- Morgan_Gregory/week_07/day_01/books/js/main.js
+++ Morgan_Gregory/week_07/day_01/books/js/main.js
@@ -3,7 +3,7 @@
 // google books api : https://www.googleapis.com/books/v1/volumes?q=title
 // might be a path to image thumbnail from the ruby books homework: ['items'][0]['volumeInfo']['imageLinks']['thumbnail']
 
-const getBookCover = function () {
+const getBook = function () {
   let title = document.getElementById('search').value
 
   const xhr = new XMLHttpRequest();
@@ -16,10 +16,25 @@ const getBookCover = function () {
     const book = JSON.parse(xhr.responseText);
     console.log(book);
 
+    // add image of cover to page
     const img = document.createElement('img');
-
     img.src = book.items[0].volumeInfo.imageLinks.thumbnail;
     document.body.appendChild(img);
+
+    // add author to page
+    const authors = document.createElement('p');
+    authors.innerHTML = book.items[0].volumeInfo.authors;
+    document.body.appendChild(authors);
+
+    // add description to page
+    const description = document.createElement('p');
+    description.innerHTML = book.items[0].volumeInfo.description;
+    document.body.appendChild(description);
+
+    // add publisher to page
+    const publisher = document.createElement('p');
+    publisher.innerHTML = book.items[0].volumeInfo.publisher;
+    document.body.appendChild(publisher);
   };
 
 
@@ -28,4 +43,4 @@ const getBookCover = function () {
 };
 
 const button = document.getElementById('go');
-button.addEventListener('click', getBookCover);
+button.addEventListener('click', getBook);
