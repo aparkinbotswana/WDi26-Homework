774220711f65e82d28114f206d1ca31c7ec20919
robo2323 <rob.adam.h@gmail.com>
2018-04-11 20:31:29 +1000

    week 7 day 3, api, weather

---
+++ Rob_Harkness/week_07/day_03/weather/js/main.js
@@ -0,0 +1,86 @@
+const qs = (query) => document.querySelector(query);
+const createEl = (el) => document.createElement(el);
+const appendEl = (child, parent) => parent.appendChild(child);
+
+const showWeather = async () => {
+  const data = await getWeather();
+  const weatherData = data.weatherData;
+  const icons = new Skycons({
+    color: 'white',
+    resizeClear: true
+  });
+  const iconNow = data.weatherData.currently.icon;
+  const dateNow = new Date(data.weatherData.daily.data[0].time * 1000).toDateString().split(' ');
+  const bgImages = {
+    'clear-day': '../img/clear-day.jpg',
+    'clear-night': '../img/clear-night.jpg',
+    cloudy: '../img/cloudy.jpg',
+    'partly-cloudy-day': '../img/partly-cloudy-day.jpg',
+    rain: '../img/rain.jpeg',
+    snow: '../img/snow.jpg',
+    sleet: '../img/snow.jpg',
+    wind: '../img/wind.jpg',
+    fog: '../img/fog.jpg',
+    'partly-cloudy-night': '../img/partly-cloudy-night.jpg'
+  };
+  qs('body').style.backgroundImage = `url(${bgImages[iconNow]})`;
+  const weatherDiv = qs('#weather');
+
+  const dateEl = createEl('h3');
+  dateEl.textContent = `${dateNow[0]}, ${dateNow[1]} ${dateNow[2]}`;
+
+  const locationEl = createEl('p');
+  locationEl.textContent = data.formattedAddress;
+
+  const currentlyEl = createEl('h2');
+  currentlyEl.textContent = weatherData.currently.summary;
+
+  const iconDiv = createEl('div');
+  const iconEl = createEl('canvas');
+  iconEl.id = 'icon-now';
+  appendEl(iconEl, iconDiv);
+
+  const tempEl = createEl('h2');
+  tempEl.textContent = `${Math.round(weatherData.currently.temperature)}°C`;
+  const tempMinMaxEl = createEl('p');
+  tempMinMaxEl.innerHTML = `Min: <span class="min">${Math.round(
+    weatherData.daily.data[0].temperatureMin
+  )}°C</span> / Max: <span class="max">${Math.round(weatherData.daily.data[0].temperatureMax)}°C</span>`;
+
+  const feelsLikeEl = createEl('p');
+  feelsLikeEl.textContent = `Feels like: ${Math.round(weatherData.currently.apparentTemperature)}°C`;
+
+  appendEl(dateEl, weatherDiv);
+  appendEl(locationEl, weatherDiv);
+  appendEl(currentlyEl, weatherDiv);
+  appendEl(iconEl, weatherDiv);
+  appendEl(tempEl, weatherDiv);
+  appendEl(tempMinMaxEl, weatherDiv);
+  appendEl(feelsLikeEl, weatherDiv);
+
+  icons.set('icon-now', iconNow);
+  icons.play();
+};
+
+const getWeather = async () => {
+  const locationUrl = 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBW9MkinNsa0YbNZo_4SYrWFCo5rGswrMs';
+
+  const location = await fetch(locationUrl, { method: 'POST' }).then((res) => res.json());
+
+  const weatherUrl = `https://cors-anywhere.herokuapp.com/https://api.forecast.io/forecast/206bf30a98c8331e37e68b65ff38ef33/${
+    location.location.lat
+  },${location.location.lng}?units=si`;
+
+  const addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.location.lat},${
+    location.location.lng
+  }&key=AIzaSyBW9MkinNsa0YbNZo_4SYrWFCo5rGswrMs`;
+
+  const address = await fetch(addressUrl).then((res) => res.json());
+  const formattedAddress = address.results[0].formatted_address;
+
+  const weatherData = await fetch(weatherUrl).then((res) => res.json());
+
+  return { weatherData, formattedAddress };
+};
+
+document.addEventListener('DOMContentLoaded', showWeather);
