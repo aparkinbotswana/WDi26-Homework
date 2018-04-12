e2686228a59aeafb1143030ff625c3703a27603e
James Kim <jameskim@Jamess-MacBook-Pro-2.local>
2018-04-13 09:17:02 +1000

    mta update

--- James_Kim/week_01/day_05/js/mta-single.js
+++ James_Kim/week_01/day_05/js/mta-single.js
@@ -10,7 +10,7 @@ const planTrip = function (start, stop) { // function to set up tripStart and tr
   tripEnd = stop;
 }
 
-planTrip('Times Square', '8th');
+planTrip('Union Square', 'Times Square');
 
 const trip = function (start, stop) {
   let stops = lineN.indexOf(start) - lineN.indexOf(stop); // local variable 'stops' gains index number of specific station and finds out the number of stations in between
@@ -19,21 +19,22 @@ const trip = function (start, stop) {
 } return stops; // returns the stops number as a positive number once the IF function has run for a negative number to convert it to a positive number
 }
 
-// 'trip' variable is used to show how many stops between specific stations
+// 'trip' function is used to show how many stops between specific stations
 console.log(`There are ${trip(tripStart, tripEnd)} stops in total between ${tripStart} and ${tripEnd}.`);
 
 const stations = function (start, stop) {
- let startStation = lineN.indexOf(start);
+ let startStation = lineN.indexOf(start); // convert station into a number
  let stopStation = lineN.indexOf(stop);
- let firstStation = lineN.indexOf(start) + 1;
+ let firstStation = lineN.indexOf(start) + 1; // plus 1 to allow slice to include second to last station
  let endStation = lineN.indexOf(stop) + 1;
  if (stopStation >= startStation) {
    let stops = lineN.slice(firstStation, stopStation);
    return stops;
  } else {
-   let stops = (lineN.slice(endStation, startStation)).reverse();
+   let stops = (lineN.slice(endStation, startStation)).reverse(); // reverse used for backward trips back to Times Square
    return stops;
  }
 }
 
+// 'stations' function used to show specific stations for duration of trip
 console.log(`You must travel from ${tripStart} through the following stops on the N Line: ${stations(tripStart, tripEnd)} to get to ${tripEnd}.`);
