ced1ac0cc53803ab2cdbf8bbebdd216fd5d8991a
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-04-17 10:38:26 +1000

    react api

---
+++ Gurdeep_Kaur/week_08/day_01/flickrsearchapi/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FlickrApi from './FlickrApi';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FlickrApi />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
